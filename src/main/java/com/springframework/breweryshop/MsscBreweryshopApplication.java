d14c09d3a1ddc568e807f6de08caaecee122ba8c
manueldvr <manueldvr@yahoo.com>
2023-05-09 12:49:00 -0300

    First  commit
    First Commit : unzip from SInitializr
    Spring Web, Lombok, DevTools, and OPS.

---
+++ src/main/java/com/springframework/breweryshop/MsscBreweryshopApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.breweryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/breweryshop/MsscBreweryshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.breweryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
